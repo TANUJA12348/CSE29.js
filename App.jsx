55411ec283deeb692e05e2c1d2e686b3f15ca606
TANUJA12348 <tanujajlp@gmail.com>
2026-04-19 20:23:08 +0530

    Experiment

--- App.jsx
+++ App.jsx
@@ -1,25 +1,27 @@
-import React from "react";
+import { useState } from "react";
 import "./App.css";
 
-// Student Component
-function Student({ name, roll, branch }) {
-  return (
-    <div className="card">
-      <h2>{name}</h2>
-      <p>Roll No: {roll}</p>
-      <p>Branch: {branch}</p>
-    </div>
-  );
-}
-
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="container">
-      <h1>Student Details</h1>
+      <h2>React Counter Application</h2>
+      <h1>{count}</h1>
 
-      <Student name="Tanuja Singh" roll="1230" branch="CSE" />
-      <Student name=" swastika singh " roll="1240" branch="IT" />
-      <Student name=" Tanishka Gupta" roll="1250" branch="ECE" />
+      <div className="btn-group">
+        <button onClick={() => setCount(count + 1)}>
+          Increment (+)
+        </button>
+
+        <button onClick={() => setCount(count - 1)}>
+          Decrement (-)
+        </button>
+      </div>
+
+      <button className="reset" onClick={() => setCount(0)}>
+        Reset
+      </button>
     </div>
   );
 }
